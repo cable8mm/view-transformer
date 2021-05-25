364e86f458a43447580be69df745447dded025fb
Sam Lee <cable8mm@gmail.com>
2021-05-25 15:12:35 +0900

    Add addBanner method

--- src/WordBinder.php
+++ src/WordBinder.php
@@ -17,4 +17,21 @@ class WordBinder
         $content = preg_replace("/\[embed\].+\/([^\?]+)\??.*\[\/embed\]/", '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/$1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', $content);
         return str_replace('<p>&nbsp;</p>', '', $content);
     }
+
+    public static function addBanner($content, $bannerHtml, $nthPTag = 0)
+    {
+        if ($nthPTag == 0) {
+            return $bannerHtml.$content;
+        }
+
+        $counter = 1;
+
+        return preg_replace_callback('/<\/p>/', function ($m) use (&$counter, $bannerHtml, $nthPTag) {
+            if ($counter++ == $nthPTag) {
+                return '</p>'.$bannerHtml;
+            }
+
+            return $m[0];
+        }, $content);
+    }
 }
--- tests/unit/WordBinderTest.php
+++ tests/unit/WordBinderTest.php
@@ -20,4 +20,17 @@ class WordBinderTest extends TestCase
 
         $this->assertEquals('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/watch" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', $view);
     }
+
+    public function testExpectAddBanner()
+    {
+        $bannerHtml = '<img src="/images/banner/banner_donation_720x200.png">';
+        $view = WordBinder::addBanner('<h1>title</h1><p>Sentence1</p><p>Sentence2</p><p>Sentence3</p><p>Sentence4</p>', $bannerHtml);
+        $this->assertEquals('<img src="/images/banner/banner_donation_720x200.png"><h1>title</h1><p>Sentence1</p><p>Sentence2</p><p>Sentence3</p><p>Sentence4</p>', $view);
+
+        $view = WordBinder::addBanner('<h1>title</h1>'.PHP_EOL.'<p>Sentence1</p>'.PHP_EOL.'<p>Sentence2</p><p>Sentence3</p><p>Sentence4</p>', $bannerHtml, 1);
+        $this->assertEquals('<h1>title</h1>'.PHP_EOL.'<p>Sentence1</p><img src="/images/banner/banner_donation_720x200.png">'.PHP_EOL.'<p>Sentence2</p><p>Sentence3</p><p>Sentence4</p>', $view);
+
+        $view = WordBinder::addBanner('<h1>title</h1>'.PHP_EOL.'<p>Sentence1</p>'.PHP_EOL.'<p>Sentence2</p><p>Sentence3</p><p>Sentence4</p>', $bannerHtml, 2);
+        $this->assertEquals('<h1>title</h1>'.PHP_EOL.'<p>Sentence1</p>'.PHP_EOL.'<p>Sentence2</p><img src="/images/banner/banner_donation_720x200.png"><p>Sentence3</p><p>Sentence4</p>', $view);
+    }
 }
